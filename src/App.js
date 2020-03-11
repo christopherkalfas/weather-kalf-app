930878d80fabbf723752b7fe49dc2448a80613c7
Chris Kalfas <christopherkalfas22@gmail.com>
2020-03-11 09:54:53 -0400

    Added a filter to the API return. By using data lists dt_txt prop, I was able to get the API to return just 1 reading per day for 5 days.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import apiConfig from './apiKeys'
 const weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?zip=10302,us&units=imperial&APPID=' + apiConfig.apikey
 class App extends React.Component {
   state = {
-    days:[]
+    fullData: [],
+    dailyWeatherData: []
   }
 
   componentDidMount = () => {
     fetch(weatherURL)
     .then(res => res.json())
     .then(data => {
-      console.log("data load", data.list)
+      //console.log("data load", data.list)
       const dailyWeatherData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
-      this.setState({days: dailyWeatherData})
+      this.setState({
+        fullData: data.list,
+        dailyWeatherData: dailyWeatherData
+      }, () => console.log(this.state))
     })
   }
+
+  // formatCards = () => {
+  //   return this.state.days.map( ( day, index ) => <Card day={day} key= {index} />)
+  // }
 
   render(){
     return(
